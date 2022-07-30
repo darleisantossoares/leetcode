215d497d323d0137f6b1b3b4db3272d1b145d2d2
Darlei Soares <darleisantossoares@gmail.com>
2022-07-30 18:43:57 -0300

    add new problem

---
+++ problems/347_top_k_frequent_elements.py
@@ -0,0 +1,7 @@
+class Solution:
+    def topKFrequent(self, nums: List[int]: k: int) -> List[int]:
+        if k == len(nums):
+            return nums
+        counter = collections.Counter(nums)
+        return heapq.nlargest(k, counter.keys(), key=counter.get)
+
