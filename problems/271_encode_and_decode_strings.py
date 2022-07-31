3b6e04a749c65ec18f44db0ca1514d92b997abf2
Darlei Soares <darleisantossoares@gmail.com>
2022-07-31 12:53:46 -0300

    add solution 2 for encode and decode string problem

--- problems/271_encode_and_decode_strings.py
+++ problems/271_encode_and_decode_strings.py
@@ -3,3 +3,22 @@ class Codec:
         return 'ç'.join(strs)
     def decode(self, s:str) -> List[str]:
         return s.split('ç')
+
+## Solution 2
+
+class Codec2:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        return ''.join('%d:' % len(s) + s for s in strs)
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        strs = []
+        i = 0
+        while i < len(s):
+            j = s.find(':', i)
+            i = j + 1 + int(s[i:j])
+            strs.append(s[j+1:i])
+        return strs
